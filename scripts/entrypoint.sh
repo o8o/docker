56462069e45fb9f22bdeb02f39180cc37f20d7cf
Andrea Lambruschini <andrea@localhost.localdomain>
2016-04-14 10:55:51 -0700

    uso solo utente builder

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,8 @@
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
-envsubst < ${HOME}/passwd.template > /tmp/passwd
-export LD_PRELOAD=/usr/local/lib64/libnss_wrapper.so
-export NSS_WRAPPER_PASSWD=/tmp/passwd
-export NSS_WRAPPER_GROUP=/etc/group
+echo "maven-3-jdk-8-builder: entrypoint.sh: user id: ${USER_ID} - group id: ${GROUP_ID} "
+#envsubst < ${HOME}/passwd.template > /tmp/passwd
+#export LD_PRELOAD=/usr/local/lib64/libnss_wrapper.so
+#export NSS_WRAPPER_PASSWD=/tmp/passwd
+#export NSS_WRAPPER_GROUP=/etc/group
 
